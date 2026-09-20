32a3633c63a02a63cd81ff01ed337bc766d38a82
rowanwebber <rowanwebber.0611@gmail.com>
2026-09-20 02:31:38 -0700

    chore: trigger Worker source deployment

--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
 // CFnew - 终端 v3.0
 // 版本: v3.0 
+// Deployment source: youxin666/cfnew-rowan
 import { connect as 连接 } from 'cloudflare:sockets';
 const 基础64文本解码器 = new TextDecoder();
 function 解码64(文本) {
